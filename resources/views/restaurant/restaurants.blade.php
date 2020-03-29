dd2d8d8d1f22a3b9f5310977d12e4891f67cadd3
anton <ac155@alu.ua.es>
2020-03-29 11:16:57 +0200

    nothing important

--- resources/views/restaurant/restaurants.blade.php
+++ resources/views/restaurant/restaurants.blade.php
@@ -1,15 +1,27 @@
 @extends('app')
 
 @section('title', 'Restaurants')
+@section('stylesheet', 'css/restaurants.css')
 
 @section('content')
     <h1>X restaurants</h1>
 
-    <ul>
-        @forelse($listRestaurants as $restaurant)
-            <li>{{ $restaurant->name }}</li>
-        @empty
-            <li>No restaurants available!</li>
-        @endforelse
-    </ul>
+    <div style="width:800px;
+                margin: auto;
+                padding: 10px">
+        <div id="_address">
+            Calle de San Lorenzo
+        </div>
+        
+        <div style="width:300px; float:right;">
+            <ul>
+                @forelse($listRestaurants as $restaurant)
+                    <li>{{ $restaurant->name }}</li>
+                @empty
+                    <li>No restaurants available!</li>
+                @endforelse
+            </ul>
+        </div>
+      </div>
+    
 @endsection
