5166f5b003a1a8855547d7d61167503d14c197ac
Amr-Hamcho <hamcho95@gmail.com>
2020-03-30 15:25:54 +0200

    signup-login

---
+++ resources/views/registration/create.blade.php
@@ -0,0 +1,107 @@
+
+<!DOCTYPE html>
+@include('navbar');
+<html>
+ <head>
+  <title>Simple Login System in Laravel</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style type="text/css">
+   .box{
+    width:600px;
+    margin:0 auto;
+    border:1px solid #ccc;
+   }
+  </style>
+ </head>
+ <body>
+  <br />
+  <div class="container box">
+   <h3 align="center">Register</h3><br />
+
+   
+
+   @if ($message = Session::get('error'))
+   <div class="alert alert-danger alert-block">
+    <button type="button" class="close" data-dismiss="alert">×</button>
+    <strong>{{ $message }}</strong>
+   </div>
+   @endif
+
+   @if (count($errors) > 0)
+    <div class="alert alert-danger">
+     <ul>
+     @foreach($errors->all() as $error)
+      <li>{{ $error }}</li>
+     @endforeach
+     </ul>
+    </div>
+   @endif
+
+   <form method="post" action="{{ url('/register') }}">
+    {{ csrf_field() }}
+    <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" class="form-control" id="name" name="name">
+        </div>
+ 
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" class="form-control" id="email" name="email">
+        </div>
+ 
+        <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" class="form-control" id="password" name="password">
+        </div>
+ 
+        <div class="form-group">
+            <label for="password_confirmation">Password Confirmation:</label>
+            <input type="password" class="form-control" id="password_confirmation"
+                   name="password_confirmation">
+        </div>
+ 
+        <div class="form-group">
+            <button style="cursor:pointer" type="submit" class="btn btn-primary">Submit</button>
+        </div>
+        
+    </form>
+    </div>
+   </form>
+  </div>
+
+  
+ </body>
+
+</html>
+<!-- <!DOCTYPE html>
+<html>
+<body>
+ 
+    <h2>Register</h2>
+    <form method="POST" action="/register">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" class="form-control" id="name" name="name">
+        </div>
+ 
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" class="form-control" id="email" name="email">
+        </div>
+ 
+        <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" class="form-control" id="password" name="password">
+        </div>
+ 
+        <div class="form-group">
+            <button style="cursor:pointer" type="submit" class="btn btn-primary">Submit</button>
+        </div>
+        
+    </form>
+ 
+</body>
+</html> -->
