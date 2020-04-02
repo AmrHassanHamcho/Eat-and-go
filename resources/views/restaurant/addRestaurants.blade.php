7eb88d24e8cd82be5fd52b746b54aef06961cb07
anton <ac155@alu.ua.es>
2020-04-02 10:10:21 +0200

    push to develoo

--- resources/views/restaurant/addRestaurants.blade.php
+++ resources/views/restaurant/addRestaurants.blade.php
@@ -45,7 +45,15 @@
                     value="read">Read
                 </button>
 
-                
+                @if ($errors->any() > 0)
+                    <div class="error-list">
+                        <ul>
+                            @foreach($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
 
 
             <!--
