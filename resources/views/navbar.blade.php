f6a01e1c76419dc629a1fcc52aef5826aaf19411
Carlos Arismendi <ceas1@alu.ua.es>
2020-05-16 11:10:42 +0200

    added the button -home- to redirect to Welcome page. Now the logo also redirects to welcome page

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/address"><img height="45" width="45" src="/img/justeat.png" alt="">
+    <a class="navbar-brand" href="/"><img height="45" width="45" src="/img/justeat.png" alt="">
         Eat & Go</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -7,6 +7,9 @@
   
     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
       <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link border-down" href="/">Home</a>
+        </li>
         <li class="nav-item active">
           <a class="nav-link border-down" href="/restaurants">Restaurants <span class="sr-only">(current)</span></a>
         </li>
