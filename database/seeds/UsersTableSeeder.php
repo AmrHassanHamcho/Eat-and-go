a336563f49f6e046440f91efa6be05293bab8d7f
Carlos Arismendi <ceas1@alu.ua.es>
2020-05-22 09:02:34 +0200

    Added a new record for users seeder and fixed a minimal bug in the login view

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,6 +16,13 @@ class UsersTableSeeder extends Seeder
         $faker = Faker::create(User::class);
         DB::table('users')->delete();
         
+        DB::table('users')->insert([            
+            'name'=> 'superadmin',
+            'email'=>'superadmin@gmail.com',
+            'password'=>\bcrypt('admin'),            
+            'role_id'=>1 // AdminApp
+        ]);
+
         for($i = 1; $i <= 10; $i++){                        
             DB::table('users')->insert([            
                 'name'=> "admin".$i,
@@ -32,72 +39,6 @@ class UsersTableSeeder extends Seeder
                 'password'=>\bcrypt('client'),            
                 'role_id'=>3 // Client
             ]);
-        }
-
-        /*
-        DB::table('users')->insert([
-            'id'=>2,
-            'name'=> 'admin2',
-            'email'=>'admin2@email.com',
-            'password'=>\bcrypt('admin'),            
-            'role_id'=>2 // AdminRestaurant
-        ]);
-
-        DB::table('users')->insert([
-            'id'=>3,
-            'name'=> 'admin3',
-            'email'=>'admin3@email.com',
-            'password'=>\bcrypt('admin'),            
-            'role_id'=>2 // AdminRestaurant
-        ]);        
-
-        DB::table('users')->insert([
-            'id'=>4,
-            'name'=> 'superadmin',
-            'email'=>'superadmin@gmail.com',
-            'password'=>\bcrypt('superadmin'),            
-            'role_id'=>1 // AdminApp
-        ]);
-
-        DB::table('users')->insert([
-            'id'=>5,
-            'name'=> 'Juan Ramón',
-            'email'=>'juanramon@gmail.com',
-            'password'=>\bcrypt('client'),            
-            'role_id'=>3 // Client
-        ]);
-
-        DB::table('users')->insert([
-            'id'=>6,
-            'name'=> 'Francisco Calvo',
-            'email'=>'franciscocalvo@gmail.com',
-            'password'=>\bcrypt('client'),            
-            'role_id'=>3 // Client
-        ]);
-
-        DB::table('users')->insert([
-            'id'=>7,
-            'name'=> 'Enrique Fernando',
-            'email'=>'enriquefernando@gmail.com',
-            'password'=>\bcrypt('client'),            
-            'role_id'=>3 // Client
-        ]);
-
-        DB::table('users')->insert([
-            'id'=>8,
-            'name'=> 'Roberto Carlos',
-            'email'=>'roberlocarlos@gmail.com',
-            'password'=>\bcrypt('client'),            
-            'role_id'=>3 // Client
-        ]);
-/*
-        $p9 = new User;
-        $p9->name="test";
-        $p9->email="test@gmail.com";
-        $p9->password="1234";
-        $p9->role_id=3;
-        $p9->save();
-*/
-
+        }                
     }
 }
